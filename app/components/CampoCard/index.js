cb0914d95eedfc085f2b659f5d269c9e8cc1204d
marlon-bento <marlon_lion@hotmail.com>
2024-04-18 00:04:39 -0300

    esqueci de colocar o titulo do card mas arrumei

--- app/components/CampoCard/index.js
+++ app/components/CampoCard/index.js
@@ -30,18 +30,18 @@ export default function CampoCard() {
 
 
                 <div id="slider" className=" flex items-center gap-3 w-full h-full overflow-x-scroll scroll scroll-smooth ">
-                    <CardParceiros imagem="/cardParceiros/card1.png" texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci qui distinctio eligendi, accusamus a!'} />
+                    <CardParceiros imagem="/cardParceiros/card1.png" texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci qui distinctio eligendi, accusamus a!'} titulo="Lorem ipsun"/>
 
-                    <CardParceiros imagem="/cardParceiros/card1.png" texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci qui distinctio eligendi, accusamus a!'} />
+                    <CardParceiros imagem="/cardParceiros/card1.png" texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci qui distinctio eligendi, accusamus a!'} titulo="Lorem ipsun"/>
 
-                    <CardParceiros imagem="/cardParceiros/card1.png" texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci qui distinctio eligendi, accusamus a!'} />
+                    <CardParceiros imagem="/cardParceiros/card1.png" texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci qui distinctio eligendi, accusamus a!'} titulo="Lorem ipsun"/>
 
-                    <CardParceiros imagem="/cardParceiros/card1.png" texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci qui distinctio eligendi, accusamus a!'} />
+                    <CardParceiros imagem="/cardParceiros/card1.png" texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci qui distinctio eligendi, accusamus a!'} titulo="Lorem ipsun"/>
 
-                    <CardParceiros imagem="/cardParceiros/card1.png" texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci qui distinctio eligendi, accusamus a!'} />
+                    <CardParceiros imagem="/cardParceiros/card1.png" texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci qui distinctio eligendi, accusamus a!'} titulo="Lorem ipsun"/>
 
 
-                    <CardParceiros imagem="/cardParceiros/card1.png" texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci qui distinctio eligendi, accusamus a!'} />
+                    <CardParceiros imagem="/cardParceiros/card1.png" texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci qui distinctio eligendi, accusamus a!'} titulo="Lorem ipsun"/>
                 </div>
 
                 <div onClick={slideRigth} className="justify-center h-72 flex items-center cursor-pointer">
