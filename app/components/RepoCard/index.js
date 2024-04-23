e36850d8d3c79450b81c47518a3cdb271a471556
NilsonDeon <nilsondeon01@gmail.com>
2024-04-23 19:13:18 -0300

    Ajustar estilos e layout da página Labs

--- app/components/RepoCard/index.js
+++ app/components/RepoCard/index.js
@@ -3,12 +3,12 @@ import BtnLink from "../BtnLink";
 
 const RepoCard = ({ repo }) => {
   return (
-    <div className="card sm:w-auto md:w-4/6 lg:w-4/8 h-48" key={repo.id}>
+    <div className="card sm:w-auto lg:w-4/6 h-auto" key={repo.id}>
       <p className="text-wrap">
         <strong className="font-bold text-primaria">{repo.name} |</strong>{" "}
         {repo.description}
       </p>
-      <BtnLink href={repo.html_url}>Ver no GitHub</BtnLink>
+      <BtnLink href={repo.html_url}></BtnLink>
     </div>
   );
 };
--- app/labs/page.js
+++ app/labs/page.js
@@ -1,46 +1,55 @@
 import Image from "next/image";
+import './style.css';
 import RepoCard from "../components/RepoCard";
-import Github from "../../github.js"
+import Github from "../../github.js";
 
 export default async function Labs() {
   let { data } = await Github.rest.repos.listForOrg({
-    org: 'WebTech-PUC-Minas'
+    org: "WebTech-PUC-Minas",
   });
+
+  data.sort((a, b) => b.stargazers_count - a.stargazers_count);
+
+  const addedRepoIds = new Set();
 
   return (
     <div className="main-labs">
       <section className="flex flex-col items-center">
-        <h1 className="title-labs text-3xl font-bold text-secundaria text-center">Principais Labs</h1>
-        <div className="wrapper-cards grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4 md:gap-12 flex flex-col items-center">
-        {
-          data
-          .filter((repo) => repo.name.startsWith('lab-'))
-          .slice(0, 4)
-          .map((repo) => (
-            <RepoCard key={repo.id} repo={repo} />
-          )) 
-        }
-)
+        <h1 className="title-labs text-3xl font-bold text-secundaria text-center">
+          Principais Labs
+        </h1>
+        <div className="wrapper-cards">
+          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4 md:gap-12 justify-center">
+            {data.slice(0, 4).map((repo) => {
+              if (!addedRepoIds.has(repo.id)) {
+                addedRepoIds.add(repo.id);
+                return <RepoCard key={repo.id} repo={repo} />;
+              }
+              return null;
+            })}
+          </div>
         </div>
       </section>
 
       <section className="more-labs flex flex-col items-center">
         <div className="wrapper-text text-left">
-          <h1 className="title-labs text-3xl font-bold text-secundaria text-center">Outros labs</h1>
-          
-          {
-            data
-            .filter((repo) => repo.name.startsWith('lab-'))
-            .map((repo) => (
-              <div key={repo.id}>
-                <p className="text-wrap">
-                  <strong className="font-bold text-primaria">{repo.name}</strong>{" "}
-                  {repo.description}
-                </p>
-              </div>
-            )) 
-          }
+          <h1 className="title-labs text-3xl font-bold text-secundaria text-center">
+            Outros labs
+          </h1>
 
+          {data.map(
+            (repo) =>
+              !addedRepoIds.has(repo.id) && (
+                <div key={repo.id}>
+                  <p className="text-wrap">
+                    <strong className="font-bold text-primaria">
+                      {repo.name}
+                    </strong>{" "}
+                    {repo.description}
+                  </p>
+                </div>
+              )
+          )}
         </div>
       </section>
     </div>
