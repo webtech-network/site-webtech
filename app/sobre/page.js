c4b85dd18f0093e8b3d9268abb0a1547a3d67fd7
Luca Azalim <lucaazalim@gmail.com>
2024-04-25 14:46:22 -0300

    Trabalhando na página de sobre.

--- app/sobre/page.js
+++ app/sobre/page.js
@@ -2,38 +2,36 @@ export default function AboutPage() {
   return (
     <main className="px-10 md:px-20">
       <div className="container mx-auto">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="">
+            <h1 className="text-4xl mt-10 mb-7 text-center font-bold">Sobre</h1>
+            <p className="text-xl">
+              A WebTech é um projeto de extensão da PUC Minas que tem como objetivo
+              gerar espaço de trabalho para que alunos e professores criem juntos
+              aplicações, ferramentas e conteúdo para interessados em criar soluções
+              e produtos digitais.
+            </p>
+          </div>
+          <div className="">
+            <h1 className="text-4xl mt-10 mb-7 text-center font-bold">Propósito</h1>
+            <p className="text-xl">
+              O propósito da WebTech é promover a troca de conhecimento e experiência
+              entre alunos e professores, para que juntos possam criar soluções inovadoras
+              e tecnológicas para a sociedade.
+            </p>
+          </div>
+        </div>
+        <h1 className="text-4xl mt-10 mb-7 text-center font-bold">Conquistas</h1>
         <div className="max-w-screen-2xl m-auto text-secundariaDark	">
-          <div className="">
-            <h1 className="sm:text-4xl sm:my-[74px] my-5 text-xl text-center font-bold ">Sobre</h1>
-            <h3 className="sm:text-3xl text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived
-              not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
-              of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h3>
-          </div>
-          <div className="">
-            <h1 className="sm:text-4xl sm:my-[74px] my-5 text-xl text-center font-bold ">Propósito</h1>
-            <h3 className="sm:text-3xl text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived
-              not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
-              of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h3>
-          </div>
-          <div className="sm:pb-20 pb-10">
-            <h1 className="sm:text-4xl sm:my-[74px] my-5 text-xl text-center font-bold ">Conquistas</h1>
-            <h3 className="sm:text-3xl text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived
-              not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
-              of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h3>
-          </div>
-
           <div className="pb-20">
             <div className="grid grid-cols-1 w-full gap-10 md:grid-cols-4">
               <div className="">
                 <img className="w-full mb-5" src="https://via.placeholder.com/300x250" alt="placeholder" />
-                <p className="sm:text-xl text-sm font-bold">Projeto em andamento de uma loja com produtos personalizados </p>
+                <p className="sm:text-xl text-sm font-bold">Site da Pós em Tecnologias Digitais da PUC Minas</p>
               </div>
               <div className="">
                 <img className="w-full mb-5" src="https://via.placeholder.com/300x250" alt="placeholder" />
-                <p className="sm:text-xl text-sm font-bold">Projeto pessoal para auxiliar na contagem de horas dos participantes</p>
+                <p className="sm:text-xl text-sm font-bold">Evento WebTech Days - JavaScript Edition</p>
               </div>
               <div className="">
                 <img className="w-full mb-5" src="https://via.placeholder.com/300x250" alt="placeholder" />
@@ -41,7 +39,7 @@ export default function AboutPage() {
               </div>
               <div className="">
                 <img className="w-full mb-5" src="https://via.placeholder.com/300x250" alt="placeholder" />
-                <p className="sm:text-xl text-sm font-bold">Labs públicos com conhecimento adquirido no projeto para ajudar outros alunos</p>
+                <p className="sm:text-xl text-sm font-bold">Labs públicos que reúnem o conhecimento adquirido nos projetos</p>
               </div>
             </div>
           </div>
@@ -51,15 +49,15 @@ export default function AboutPage() {
             <div className="grid grid-cols-1 w-full gap-10 sm:grid-cols-3">
               <div className="">
                 <img className="w-full mb-5" src="https://via.placeholder.com/400x250" alt="placeholder" />
-                <p className="sm:text-xl text-sm font-bold">Conectar alunos em um amiente colaborátivo</p>
+                <p className="sm:text-xl text-sm font-bold">Semanalmente, nos reunimos nos espaços da WebTech nos campi Praça da Liberdade e Coração Eucarístico</p>
               </div>
               <div className="">
                 <img className="w-full mb-5" src="https://via.placeholder.com/400x250" alt="placeholder" />
-                <p className="sm:text-xl text-sm font-bold">Contato com tecnologias emergentes no mercado</p>
+                <p className="sm:text-xl text-sm font-bold">A WebTech frequentemente organiza workshops e outros eventos para a comunidade</p>
               </div>
               <div className="">
                 <img className="w-full mb-5" src="https://via.placeholder.com/400x250" alt="placeholder" />
-                <p className="sm:text-xl text-sm font-bold">Desenvolver projetos novos, e criativos</p>
+                <p className="sm:text-xl text-sm font-bold">Estamos sempre conectados com as empresas do mercado de tecnologia e com outros potenciais parceiros</p>
               </div>
             </div>
           </div>
