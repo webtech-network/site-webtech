826a9a0a431853f29b94eab3c059c10b57d07755
Luca Azalim <lucaazalim@gmail.com>
2024-04-27 01:24:47 -0300

    Adicionando contribuidores dos labs.

--- app/labs/page.js
+++ app/labs/page.js
@@ -12,6 +12,15 @@ export default async function LabsPage() {
         org: process.env.GITHUB_ORG_NAME,
     });
 
+    await Promise.all(data.map(async repo => {
+        const contributors = await Github.rest.repos.listContributors({
+            owner: repo.owner.login,
+            repo: repo.name,
+        });
+
+        repo.contributors = contributors.data;
+    }))
+
     data = data
         .filter(repo => repo.name.startsWith('lab-'))
         .sort((a, b) => b.updated_at > a.updated_at ? 1 : -1);
@@ -30,11 +39,17 @@ export default async function LabsPage() {
 }
 
 function LabCard({ repo }) {
-    console.log(repo);
     return (
-        <div className="p-4 bg-gray-100 rounded-lg overflow-hidden flex flex-col justify-between">
+        <div className="p-4 bg-gray-100 rounded-lg flex flex-col justify-between">
             <div>
-                <h5 className="text-lg font-bold mb-2">{repo.name}</h5>
+                <div className="flex justify-between gap-1">
+                    <h5 className="text-lg font-bold mb-2">{repo.name}</h5>
+                    <div className="flex justify-end gap-1">
+                        {repo.contributors.slice(0, 5).map(contributor => (
+                            <LabContributor key={contributor.id} contributor={contributor} />
+                        ))}
+                    </div>
+                </div>
                 <p>{repo.description}</p>
             </div>
             <div className="mt-5">
@@ -47,6 +62,14 @@ function LabCard({ repo }) {
         </div>
     );
 };
+
+function LabContributor({ contributor }) {
+    return (
+        <a href={contributor.html_url} target="_blank">
+            <img src={contributor.avatar_url} className="w-7 h-7 rounded-full" alt={contributor.login} />
+        </a>
+    );
+}
 
 function LabLabel({ children }) {
     return (
