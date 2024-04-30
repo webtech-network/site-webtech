b2fd3c39e9de575dd36a6bce4a49d972e7953f9b
Luca Azalim <lucaazalim@gmail.com>
2024-04-30 13:21:19 -0300

    Ajustes de interface.

--- app/components/home/Banner.js
+++ app/components/home/Banner.js
@@ -43,23 +43,23 @@ export default async function Banner() {
     const formattedDate = event ? formatDate(event.end_date) : '';
 
     return (
-        <section className="conteudo-banner container mx-auto grid gap-8 grid-cols-1 lg:grid-cols-2  justify-between">
+        <section className="w-100 p-7 rounded-2xl bg-neutral-900 container mx-auto grid gap-8 grid-cols-1 lg:grid-cols-2  justify-between">
             {error ? (
                 <p>{error}</p>
             ) : (
                 <>
                     <div className="flex flex-col justify-between col-span-4/6 ">
-                        <h1 className="text-3xl font-bold text-primary text-sombra">{event.name}</h1>
+                        <h1 className="text-3xl font-bold text-primary">{event.name}</h1>
                         <div>
-                            <p className="text-xl text-white text-sombra">{description}</p>
-                            <span className="block text-white font-bold text-sombra mb-2 mt-10">
+                            <p className="text-xl text-white">{description}</p>
+                            <span className="block text-white font-bold mb-2 mt-10">
                                 <FontAwesomeIcon icon={faCalendar} className="mr-2" />{formattedDate}
                             </span>
                         </div>
                         <ButtonLink texto="Inscreva-se" link={event.url} />
                     </div>
                     <div className="flex md:justify-end justify-center col-span-2/6">
-                        <img src={event.image} alt="" className="max-w-full rounded-xl h-auto drop-shadow-2xl imagem-banner" />
+                        <img src={event.image} alt="" className="max-w-full rounded-xl h-auto drop-shadow-2xl" />
                     </div>
                 </>
             )}
