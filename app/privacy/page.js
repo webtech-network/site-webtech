dbf1669d471709efb9ef57b175e650a8a02f169d
ygabsxw <gabrieldrvianna@gmail.com>
2024-09-06 14:04:17 -0300

    update terms&privacy

--- app/privacy/page.js
+++ app/privacy/page.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faXTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope, faGlobe } from "@fortawesome/free-solid-svg-icons";
 
 export default async function Privacy() {
 
