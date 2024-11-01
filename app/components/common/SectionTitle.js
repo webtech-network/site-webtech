65b6670ecb49071203af542ddb03d3ebbabc6e8e
Rommel Vieira Carneiro <rommelcarneiro@gmail.com>
2024-11-01 14:31:19 -0300

    Ajustes em componentes e adição da página WebTech Journey

--- app/components/common/SectionTitle.js
+++ app/components/common/SectionTitle.js
@@ -1,4 +1,4 @@
 export default function SectionTitle({ children, color = "text-secondary" }) {
-    const classNames = `text-4xl mt-10 mb-7 text-center font-bold ${color}`;
+    const classNames = `text-4xl mt-10 mb-7 text-left font-bold ${color}`;
     return <h1 className={classNames}>{children}</h1>;
 }
--- app/components/home/PartnersSection.js
+++ app/components/home/PartnersSection.js
@@ -6,7 +6,7 @@ import SectionTitle from "../common/SectionTitle";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-export default function PartnerSection() {
+export default function PartnerSection({partners}) {
 
     const slideLeft = () => {
         var slider = document.getElementById('slider');
@@ -20,8 +20,6 @@ export default function PartnerSection() {
 
     return (
         <>
-
-            <SectionTitle>Parceiros</SectionTitle>
             <div className="relative flex items-center">
 
                 <div onClick={slideLeft} className="justify-center h-72 flex items-center cursor-pointer">
@@ -29,10 +27,15 @@ export default function PartnerSection() {
                 </div>
 
                 <div id="slider" className=" flex  gap-3 w-full h-full overflow-x-scroll scroll scroll-smooth ">
-                    <PartnerCard imagem="/home/partners/sydle.png" text="A SYDLE impulsiona a transformação digital. Sua plataforma SYDLE ONE reúne soluções de BPM, CRM, E-commerce, Billing, Service Desk e mais." titulo="Sydle" />
-                    <PartnerCard imagem="/home/partners/dti.png" text="A DTI Digital é uma empresa ágil e nativa digital que oferece soluções personalizadas para acelerar a transformação digital de grandes organizações." titulo="dti digital" />
-                    <PartnerCard imagem="/home/partners/ioasys.png" text="A ioasys é uma empresa de eficiência digital que impulsiona pessoas, empresas e futuros por meio de inovações que agregam ao negócio de seus clientes." titulo="ioasys" />
-                    <PartnerCard imagem="/home/partners/waymind.png" text="A Way Minder é uma plataforma online que fortalece a resiliência emocional e previne transtornos mentais, combinando práticas de saúde mental, tecnologia e comunidade." titulo="Way Minder" />
+
+                    {partners.map((partner, index) => (
+                        <PartnerCard
+                            key={index}
+                            imagem={partner.imagem}
+                            text={partner.descricao}
+                            titulo={partner.titulo}
+                        />
+                    ))}                    
                 </div>
 
                 <div onClick={slideRigth} className="justify-center h-72 flex items-center cursor-pointer">
