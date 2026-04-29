5d7e81f9f22b520c86f73c9093edeb6e9267e51a
rhaynermartins <rhayner2005@hotmail.com>
2026-04-29 18:43:01 -0300

    teste na correcao do filtro do sympla

--- app/sympla.js
+++ app/sympla.js
@@ -1,9 +1,17 @@
 export default async function getAllEvents() {
     const response = await fetch('https://api.sympla.com.br/public/v4/events', {
         headers: {
-            's_token': process.env.SYMPLA_API_KEY,
+            s_token: process.env.SYMPLA_API_KEY,
         },
+        cache: 'no-store',
     });
+
     const data = await response.json();
-    return data;
+
+   const publicEvents = data.data.filter(event => event.private_event === 0);
+    
+    return {
+        ...data,
+        data: publicEvents
+    };
 }
